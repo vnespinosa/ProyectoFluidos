de53e05e864d5598c214c9eaa712cf80b72170b4
Vicente Espinosa <vnespinosa@uc.cl>
2020-12-06 16:24:59 -0300

    Cambiar nombre arco

--- scripts/resize.js
+++ scripts/resize.js
@@ -2,11 +2,13 @@ function ajustarPantalla(){
     let ancho = window.screen.width
     let alto = window.screen.height
 
-    let diferencia = Math.sqrt((ancho * ancho) + (alto * alto))/ Math.sqrt((1080*1080) + (1920 * 1920))
+    let diferencia = Math.sqrt((ancho * ancho) + (alto * alto))/ Math.sqrt((864*864) + (1536 * 1536))
     let porcentaje = diferencia*100
 
     let ventana = document.getElementsByTagName("body")[0]
     ventana.setAttribute("style", `zoom: ${porcentaje}%` )
+    console.log("ATLTO: " + alto)
+    console.log("ANCHO: " + ancho)
 }
 
 ajustarPantalla()
